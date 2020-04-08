2ee0e7651bbd70a1b7ec9add62fc670a28f66db8
Patrick Duque <patpatduque26@gmail.com>
2020-04-08 14:33:36 +0800

    broken home page to smaller components and add style

---
+++ src/containers/HomePage/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './HomePage.scss';
+import Directory from '../../components/Directory/Directory';
+
+const HomePage = (props) => {
+	return (
+		<div className='homepage'>
+			<Directory />
+		</div>
+	);
+};
+
+export default HomePage;
