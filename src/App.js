3d9aa76d2c196b313ecce9182fd19645a215df62
Patrick Duque <patpatduque26@gmail.com>
2020-04-08 19:51:53 +0800

    added HOC routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import HomePage from './containers/HomePage/HomePage';
 import './App.css';
+
+const HatsPage = () => <h1>Hats Page</h1>;
 
 function App() {
 	return (
 		<div className='App'>
-			<HomePage />
+			<Switch>
+				<Route path='/' component={HomePage} />
+				<Route path='/shop/hats' component={HatsPage} />
+			</Switch>
 		</div>
 	);
 }
